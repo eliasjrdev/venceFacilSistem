153f5aee7ab09cab8a2d81cbde818df05871014b
eliasjrdev <eliasjrcosta886@gmail.com>
2025-05-12 23:48:26 -0300

    criação das variaveis de ambiente

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyDlJ14HWJe9UcW5EfiQtPA5yP8RWOPGIVY",
-    authDomain: "vencefacil.firebaseapp.com",
-    projectId: "vencefacil",
-    storageBucket: "vencefacil.firebasestorage.app",
-    messagingSenderId: "951700127329",
-    appId: "1:951700127329:web:05d9b7d4b3d22c39a2f420"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
-
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const db = getFirestore(app);
